aa17532d37df3820416fe1d71d0e26203d40064b
Junmo Park <junmopark@Junmo-Parkui-MacBookPro.local>
2021-05-05 13:41:32 +0900

    Add : Mission 3 - 댓글 기능 구현

--- src/Pages/junmo/Main/components/comments.js
+++ src/Pages/junmo/Main/components/comments.js
@@ -2,16 +2,47 @@ import React from 'react';
 import './comments.scss';
 
 class Comments extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputComment: '',
+      commentList: [],
+    };
+  }
+  inputHandler = e => {
+    this.setState({
+      inputComment: e.target.value,
+    });
+  };
+  addComment = e => {
+    e.preventDefault();
+    this.setState({
+      commentList: this.state.commentList.concat(this.state.inputComment),
+      inputComment: '',
+    });
+  };
+
   render() {
     return (
-      <form className="commentBox">
-        <input
-          className="commentInput"
-          type="text"
-          placeholder="댓글 달기..."
-        />
-        <button className="commentAddBtn">게시</button>
-      </form>
+      <>
+        <div className="commentsList">
+          <ul>
+            {this.state.commentList.map(el => {
+              return <li>{el}</li>;
+            })}
+          </ul>
+        </div>
+        <form className="addComments" onSubmit={this.addComment}>
+          <input
+            value={this.state.inputComment}
+            className="commentInput"
+            type="text"
+            placeholder="댓글 달기..."
+            onChange={this.inputHandler}
+          />
+          <button className="commentAddBtn">게시</button>
+        </form>
+      </>
     );
   }
 }
